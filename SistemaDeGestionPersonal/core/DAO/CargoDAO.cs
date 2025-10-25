f208df57c4c77efdf82964307e9c75f87b63f05e
Diego Escobar <diegomelgar1456@gmail.com>
2025-10-24 23:45:21 -0300

    cargo DAO

--- SistemaDeGestionPersonal/core/DAO/CargoDAO.cs
+++ SistemaDeGestionPersonal/core/DAO/CargoDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using SistemaDeGestionPersonal.core.Clases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,30 @@ namespace SistemaDeGestionPersonal.core.DAO
 {
     internal class CargoDAO
     {
+        public List<Cargo> ObtenerTodos()
+        {
+            var lista = new List<Cargo>();
+            using (var conn = _Conexion.ObtenerConexion())
+            {
+                conn.Open();
+                using (var cmd = new SqlCommand("SELECT id, nombre, nivel, salarioBase FROM Cargo", conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lista.Add(new Cargo
+                            {
+                                Id = (int)reader["id"],
+                                Nombre = reader["nombre"].ToString(),
+                                Nivel = reader["nivel"].ToString(),
+                                SalarioBase = (decimal)reader["salarioBase"]
+                            });
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
     }
 }
